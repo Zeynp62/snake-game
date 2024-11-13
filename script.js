c6616cb1bb23c30d5f55bc6db191ded553373d46
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2024-11-13 11:52:42 +0300

    added the game map function

--- script.js
+++ script.js
@@ -7,9 +7,29 @@ let winState
 let score
 let snake
 /*------------------------ Cached Element References ------------------------*/
-
+const messageEl = document.querySelector('#message')
+const scoreEl = document.querySelector('#score')
+const reserBtn = document.querySelector('#reset')
+const boardEl = document.querySelector('#board')
 /*-------------------------------- Functions --------------------------------*/
-const init = () => {}
+const init = () => {
+  board = []
+  winState = false
+  score = 0
+  snake = []
+  food = ['0'] // placed in the same place when start
+  render()
+}
+const generateMap = () => {
+  for (let i = 0; i < 64; i++) {
+    const newDivs = document.createElement('div')
+    console.log(newDivs)
+    newDivs.classList.add('sqr')
+    newDivs.id = `${i}`
+    boardEl.appendChild(newDivs)
+  }
+}
+
 const render = () => {}
 const placeFood = () => {}
 const updateMessage = () => {}
@@ -18,3 +38,7 @@ const growSnake = () => {}
 const calculateScore = () => {}
 const movingSnake = () => {}
 /*----------------------------- Event Listeners -----------------------------*/
+window.onload = () => {
+  init()
+}
+generateMap()
